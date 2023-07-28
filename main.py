295725645296f49ba4721be19778abca93563116
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-28 09:23:34 +0300

    added a few things

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import svm
 
+spam = pd.read_csv('emails.csv')
+spam.head()
