9a27a8e5d01156a5dfea088ff715f9b9b326130e
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-29 08:54:15 +0300

    Greatness

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ from sklearn import svm
 
 spam = pd.read_csv('emails.csv')
 spam.head()
+
+
+z = spam["EmailText"]
+y = spam["Label"]
+z_train, z_test, y_train, y_test = train_test_split(z,y,test_size=0.25)
+
